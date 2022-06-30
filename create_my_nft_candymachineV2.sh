e5bf867091c0160fb4854198c85250baef5fe5d6
alee-blcksync <>
2022-06-30 00:40:47 -0700

    First version

---
+++ create_my_nft_candymachineV2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+CURR_DIR=$(cd $(dirname $0); pwd)
+
+solana --version
+solana-keygen new --no-bip39-passphrase --outfile $CURR_DIR/mynft-keypair.json
+solana-keygen new --no-bip39-passphrase --outfile $CURR_DIR/mynft-treasury.json
+solana-keygen new --no-bip39-passphrase --outfile $CURR_DIR/devnet-keypair.json
+
+deploy_wallet=$(solana-keygen pubkey $CURR_DIR/devnet-keypair.json)
+treasury_wallet=$(solana-keygen pubkey $CURR_DIR/mynft-treasury.json)
+
+solana config set --keypair $CURR_DIR/devnet-keypair.json
+
+solana config set --url https://metaplex.devnet.rpcpool.com/
+solana config get
+
+echo "Airdrop 2 SOL to $deploy_wallet"
+solana airdrop 2 $deploy_wallet
+
+rm -rf "$CURR_DIR/uploaded_assets"
+cp -rp "$CURR_DIR/assets_examples" "$CURR_DIR/uploaded_assets"
+pushd "$CURR_DIR/uploaded_assets"
+for f in $(find . -type f -name "*.json")
+do
+  sed -i $(basename $f).bak "s/REPLACEME_WITH_WALLET/$deploy_wallet/g" $f
+done
+rm -f *.bak
+popd
+
+echo "Current solana wallet is configured to $(solana address)"
+echo "Current NFT treasury wallet is configured to $treasury_wallet"
+
+if [ ! -d metaplex ] ; then
+  echo "ERROR: metaplex directory should exist after running setup.sh within the same directory! exiting!"
+  exit -1
+fi
+
+rm -rf metaplex/js/.cache
+mkdir -p metaplex/assets
+rm -f metaplex/assets/*
+cp -rp uploaded_assets/* metaplex/assets/
+rm -f metaplex/js/packages/candy-machine-ui/.env.example
+cat example-candy-machine-upload-config.json | sed "s/REPLACEME_GOLIVE_DATE/$TIMENOW/g" | sed "s/REPLACEME_TREASURY_WALLET/$treasury_wallet/g" > metaplex/js/packages/cli/example-candy-machine-upload-config.json
+TIMENOW=$(date +"%d %B %Y %H:%M:%S GMT")
+pushd metaplex/js
+yarn install
+ts-node $CURR_DIR/metaplex/js/packages/cli/src/candy-machine-v2-cli.ts \
+  upload \
+  -e devnet \
+  -k $CURR_DIR/devnet-keypair.json \
+  -cp $CURR_DIR/metaplex/js/packages/cli/example-candy-machine-upload-config.json \
+  $CURR_DIR/metaplex/assets | tee $CURR_DIR/metaplex.log
+popd
+
+# Apply Candy Machine here to apply to UI
+CANDY_MACHINE=$(grep "Candy machine address" $CURR_DIR/metaplex.log | cut -d: -f2- | tr -d ' ')
+cat js-packages-cli-candy-machine-ui.env | sed "s/REPLACEME_CANDY_MACHINE/$CANDY_MACHINE/g" > metaplex/js/packages/candy-machine-ui/.env
+
+echo "Candy Machine account is $CANDY_MACHINE"
+echo "Starting local UI localhost:3000 for you to mint, you can use Firefox/Chrome Phantom extension and mint"
+echo "Use 'solana airdrop 2 YOUR_WALLET' to airdrop 2 solana to your Phantom wallet to mint"
+
+pushd metaplex/js/packages/candy-machine-ui
+yarn install && yarn start
+popd
+
+exit 0
---
+++ setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+CURR_DIR=$(cd $(dirname $0); pwd)
+
+# Using metaplex devnet here
+SOLANA_NETWORK="https://metaplex.devnet.rpcpool.com/"
+
+solana --version
+
+# Metaplex dev environment setup
+# Doc reference: https://docs.metaplex.com/candy-machine-v2/getting-started
+
+echo "Setting up Metaplex dev environment, reference"
+echo "https://docs.metaplex.com/candy-machine-v2/getting-started"
+
+# Setting up nvm
+nvm_version=$(nvm version 2>/dev/null)
+ret=$?
+if [ $ret != "0" ] ; then
+  echo "nvm not installed, we use nvm to install Node.js and switch between version."
+  echo "installing nvm now, Ctrl+C if you don't want to run this. Comment these line out as needed."
+  brew install nvm
+  mkdir ~/.nvm
+  brew cleanup
+  export NVM_DIR="$HOME/.nvm"\n  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh" \n  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+else
+  echo "nvm version $nvm_version detected, we will use v16 here."
+fi
+nvm install 16
+nvm use 16
+nvm list
+
+# upgrade yarn version to 1.22 if applicable
+yarn_version=$(yarn --version 2>/dev/null)
+ret=$?
+if [ $ret != "0" ] ; then
+  echo "installing yarn via brew"
+  brew install yarn
+else
+  echo "detect yarn version $yarn_version"  
+fi
+
+# node version >= 16.10
+corepack enable
+
+# node version < 16.10
+# npm i -g corepack
+
+# Check installed version, node => v16.15.1, yarn => 1.17.3 as of 20220622
+node --version
+yarn --version
+
+npm install -g ts-node
+# 10.7.0
+ts-node --version
+
+# intel = x86_64
+# M1 = arm64
+HW_CPU_SPEC=$(uname -m)
+if [ "x${HW_CPU_SPEC}" != "x86_64" ] ; then
+  echo "WARNING: CPU Intel chip not detected, Apple M1 chip? additional dependencies required! See"
+  echo "https://docs.metaplex.com/candy-machine-v2/getting-started#apple-m1-chip"
+fi
+
+# use master branch
+git clone -b master https://github.com/metaplex-foundation/metaplex.git
+
